4f323fbb864fd72bb496c64832949c33007fec5b
Austin Gardner <gardneraustin@protonmail.com>
2020-11-10 13:50:22 -0800

    added dep

--- packages/makeup-typeahead/src/index.js
+++ packages/makeup-typeahead/src/index.js
@@ -1,14 +1,40 @@
 'use strict';
 
-function typeahead(el, strArray) {
-    el.addEventListener('onKeyPress', handleKeyPress);
-}
+const findIndex = require('core-js-pure/features/array/find-index');
 
-function handleKeyPress(e) {
-    console.log(e);
-    console.log(e.keyCode);
+
+let timeout;
+let typeStr = "";
+
+function typeahead(nodeList, char, timeoutLength) {
+    typeStr = typeStr.concat(char);
+    let index;
+    index = findIndex(nodeList, (el) => el.innerText.startsWith(typeStr));
+    if (index === -1) {
+        index = findIndex(nodeList, (el) => el.innerText.includes(typeStr));
+    }
+
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+
+    setTimeout(() => {
+        clearTimeout(timeout);
+        typeStr = "";
+    }, timeoutLength);
+
 }
 
 module.exports = {
     typeahead
 };
+
+// const findIndex = require('core-js-pure/features/array/find-index');
+// // returns index of first node in NodeList with matching first character (case-insenstive)
+// function findNodeWithFirstChar(nodeList, char) {
+//     return findIndex(nodeList, (el) => el.innerText.charAt(0).toLowerCase() === char.toLowerCase());
+// }
+
+// module.exports = {
+//     findNodeWithFirstChar
+// };

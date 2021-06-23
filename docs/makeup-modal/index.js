f6f712d8c76b23718ee4febbd6167c1a1c7508c9
Ian McBurnie <ianmcburnie@users.noreply.github.com>
2021-06-23 15:34:11 -0700

    docs(modal): minor cleanup

--- docs/makeup-modal/index.js
+++ docs/makeup-modal/index.js
@@ -3,7 +3,7 @@ const modalEl = document.querySelectorAll('.modal')[0];
 const button = document.querySelectorAll('button')[0];
 const modalElTwo = document.querySelectorAll('.modal')[1];
 const buttonTwo = document.querySelectorAll('button')[1];
-const unhoistButton = document.querySelectorAll('button')[2];
+const unmodalButton = document.querySelectorAll('button')[2];
 const hoistCheckbox = document.querySelector('input');
 
 let doHoist = false;
@@ -20,6 +20,6 @@ buttonTwo.addEventListener('click', () => {
     modal.modal(modalElTwo, { useHiddenProperty: false, hoist: doHoist });
 });
 
-unhoistButton.addEventListener('click', () => {
+unmodalButton.addEventListener('click', () => {
     modal.unmodal();
 });

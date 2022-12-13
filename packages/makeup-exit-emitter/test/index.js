90192674de3df39b32f89c4d045feadfd1d83be4
Goutham Ponnada <sponnada@ebay.com>
2022-12-13 15:38:34 -0800

    test(exit-emitter): refactor to support mocha

--- packages/makeup-exit-emitter/test/index.js
+++ packages/makeup-exit-emitter/test/index.js
@@ -1,3 +1,5 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
 import * as ExitEmitter from '../src/index.js';
 
 const timeoutInterval = 500;
@@ -21,33 +23,58 @@ describe('given an element with focus', function() {
         testElSibling = document.querySelector('#test-element-sibling');
         ExitEmitter.addFocusExit(testEl);
         testEl.addEventListener('onFocusExit', onFocusExit);
-        onFocusExit = jasmine.createSpy('onFocusExit');
+        onFocusExit =sinon.spy();
         testEl.focus();
     }
 
-    beforeAll(setup);
+    before(setup);
     afterEach(setup);
 
     describe('when focus moves to sibling', function() {
-        beforeAll(function() { 
+        before(function() { 
             testElSibling.focus();
         });
 
         it('should trigger focusExit once', function() {
             setTimeout(function() {
-                expect(onFocusExit).toHaveBeenCalledTimes(1); 
+                expect(onFocusExit.callCount).to.equal(1);
             }, timeoutInterval);
         });
     });
 
     describe('when focus moves to descendant', function() {
-        beforeAll(function() { 
+        before(function() { 
             testEl.querySelector('button').focus();
         });
 
         it('should not trigger focusExit', function() {
             setTimeout(function() {
-                expect(onFocusExit).not.toHaveBeenCalled();
+                expect(onFocusExit.callCount).to.equal(0); 
+            }, timeoutInterval);
+        });
+    });
+
+    describe('when focus exits with blur', function() {
+        before(function() { 
+            testEl.blur();
+        });
+
+        it('should trigger focusExit once', function() {
+            setTimeout(function() {
+                expect(onFocusExit.callCount).to.equal(1);
+            }, timeoutInterval);
+        });
+    });
+
+    describe('when focus moves to sibling without focusExit', function() {
+        before(function() { 
+            ExitEmitter.removeFocusExit(testEl);
+            testElSibling.focus();
+        });
+
+        it('should trigger focusExit once', function() {
+            setTimeout(function() {
+                expect(onFocusExit.callCount).to.equal(0);
             }, timeoutInterval);
         });
     });
@@ -68,45 +95,45 @@ describe('given an element with focus on descendant', function() {
         testElSibling = document.querySelector('#test-element-sibling');
         ExitEmitter.addFocusExit(testEl);
         testEl.addEventListener('onFocusExit', onFocusExit);
-        onFocusExit = jasmine.createSpy('onFocusExit');
+        onFocusExit =sinon.spy();
         testEl.querySelector('button').focus();
     }
 
-    beforeAll(setup);
+    before(setup);
     afterEach(setup);
 
     describe('when focus moves to sibling of element root', function() {
-        beforeAll(function() { 
+        before(function() { 
             testElSibling.focus();
         });
 
         it('should trigger focusExit once', function() {
             setTimeout(function() {
-                expect(onFocusExit).toHaveBeenCalledTimes(1); 
+                expect(onFocusExit.callCount).to.equal(1);
             }, timeoutInterval);
         });
     });
 
     describe('when focus is reset on descendant', function() {
-        beforeAll(function() { 
+        before(function() { 
             testEl.querySelector('button').focus();
         });
 
         it('should not trigger focusExit', function() {
             setTimeout(function() {
-                expect(onFocusExit).not.toHaveBeenCalled();
+                expect(onFocusExit.callCount).to.equal(0); 
             }, timeoutInterval);
         });
     });
 
     describe('when focus moves to element root', function() {
-        beforeAll(function() { 
+        before(function() { 
             testEl.focus();
         });
 
         it('should not trigger focusExit', function() {
             setTimeout(function() {
-                expect(onFocusExit).not.toHaveBeenCalled();
+                expect(onFocusExit.callCount).to.equal(0); 
             }, timeoutInterval);
         });
     });

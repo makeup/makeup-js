64355ba70be88f1d0ef313fc59e901ddd3a752f1
Goutham Ponnada <sponnada@ebay.com>
2025-01-28 14:16:29 -0800

    fix(active-desendant): item scroll when not in view (#199)

--- packages/core/makeup-active-descendant/dist/mjs/index.js
+++ packages/core/makeup-active-descendant/dist/mjs/index.js
@@ -8,6 +8,10 @@ const defaultOptions = {
   axis: "both",
   wrap: false
 };
+function isElementInView(el) {
+  const bounding = el.getBoundingClientRect();
+  return bounding.top >= 0 && bounding.left >= 0 && bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) && bounding.right <= (window.innerWidth || document.documentElement.clientWidth);
+}
 function onModelInit(e) {
   const { items, toIndex } = e.detail;
   const itemEl = items[toIndex];
@@ -27,8 +31,8 @@ function onModelChange(e) {
   if (toItem) {
     toItem.classList.add(this._options.activeDescendantClassName);
     this._focusEl.setAttribute("aria-activedescendant", toItem.id);
-    if (this._options.autoScroll && this._itemContainerEl) {
-      toItem.scrollIntoView({ block: "center" });
+    if (this._options.autoScroll && this._itemContainerEl && !isElementInView(toItem)) {
+      toItem.scrollIntoView({ behavior: "smooth", block: "nearest" });
     }
   }
   this._el.dispatchEvent(new CustomEvent("activeDescendantChange", { detail: e.detail }));

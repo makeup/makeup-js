b4bda0fd6205004f45b00f17e484695fd7ebde13
Akshay Shinde <shindea@oregonstate.edu>
2025-04-04 17:49:31 -0700

    feat(listbox, listbox-button): adding listbox and listbox-button examples with group headers (#208)

--- packages/ui/makeup-listbox-button/test/index.spec.js
+++ packages/ui/makeup-listbox-button/test/index.spec.js
@@ -110,7 +110,7 @@ test.describe("given a listbox-button for automatic selection", function () {
   test.beforeEach(async ({ page }) => {
     await page.goto("/ui/makeup-listbox-button/index.html");
 
-    containerEl = page.locator(".listbox-button[data-makeup-auto-select='true']").nth(3);
+    containerEl = page.locator(".listbox-button[data-makeup-auto-select='true']").first();
     hostBtn = containerEl.locator("button");
     contentEl = containerEl.locator(".listbox-button__listbox");
     OptionsEl = contentEl.locator(".listbox-button__options");
@@ -187,6 +187,133 @@ test.describe("given a listbox-button for automatic selection", function () {
     test("should not focus on disabled option", async function () {
       const disabledOptionEl = await contentEl.locator(".listbox-button__option[aria-disabled='true']").first();
       await expect(disabledOptionEl).not.toBeFocused();
+    });
+  });
+});
+
+test.describe("given a listbox-button for automatic selection with grouped headers", function () {
+  let containerEl;
+  let hostBtn;
+  let contentEl;
+  let OptionsEl;
+  let firstOptionEl;
+  let secondOptionEl;
+  let thirdOptionEl;
+  let fourthOptionEl;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/ui/makeup-listbox-button/index.html");
+  });
+
+  test.describe("for unselected, manual selection variant", async function () {
+    test.beforeEach(async ({ page }) => {
+      containerEl = page.locator(".listbox-button").nth(3);
+      hostBtn = containerEl.locator("button");
+      contentEl = containerEl.locator(".listbox-button__listbox");
+      OptionsEl = contentEl.locator(".listbox-button__options");
+      firstOptionEl = contentEl.locator(".listbox-button__option").first();
+      secondOptionEl = contentEl.locator(".listbox-button__option").nth(1);
+    });
+    test("should have the correct initial state", async function () {
+      expect(containerEl).toBeTruthy();
+      await expect(hostBtn).toHaveAttribute("aria-haspopup", "listbox");
+      await expect(hostBtn).toHaveAttribute("aria-expanded", "false");
+      await expect(contentEl).not.toBeVisible();
+      await expect(OptionsEl).toHaveAttribute("role", "listbox");
+      await expect(firstOptionEl).toHaveAttribute("role", "option");
+    });
+
+    test("should expand on host click", async function () {
+      await hostBtn.click();
+      await expect(hostBtn).toHaveAttribute("aria-expanded", "true");
+      await expect(contentEl).toBeVisible();
+    });
+
+    test("should use valid aria on selection", async function () {
+      await hostBtn.click();
+      await expect(secondOptionEl).toBeVisible();
+      await secondOptionEl.click();
+      await expect(secondOptionEl).toHaveAttribute("aria-selected", "true");
+    });
+
+    test("should not expand on host focus", async function () {
+      await hostBtn.focus();
+      await expect(hostBtn).toHaveAttribute("aria-expanded", "false");
+    });
+
+    test("should expand on host using kb [space] key", async function () {
+      await hostBtn.focus();
+      await hostBtn.press("Space");
+      await expect(hostBtn).toHaveAttribute("aria-expanded", "true");
+    });
+
+    test("should expand on host using kb [enter] key", async function () {
+      await hostBtn.focus();
+      await hostBtn.press("Enter");
+      await expect(hostBtn).toHaveAttribute("aria-expanded", "true");
+    });
+
+    test("should dismiss content on [escape] key", async function () {
+      await hostBtn.focus();
+      await hostBtn.press("Space");
+      await hostBtn.press("Escape");
+      await expect(hostBtn).toHaveAttribute("aria-expanded", "false");
+    });
+
+    test("should not focus on disabled option", async function () {
+      const disabledOptionEl = await contentEl.locator(".listbox-button__option[aria-disabled='true']").first();
+      await expect(disabledOptionEl).not.toBeFocused();
+    });
+  });
+
+  test.describe("for selected, automatic selection variant", async function () {
+    test.beforeEach(async ({ page }) => {
+      containerEl = page.locator(".listbox-button").nth(11);
+      hostBtn = containerEl.locator("button");
+      contentEl = containerEl.locator(".listbox-button__listbox");
+      OptionsEl = contentEl.locator(".listbox-button__options");
+      firstOptionEl = OptionsEl.locator(".listbox-button__option").first();
+      secondOptionEl = OptionsEl.locator(".listbox-button__option").nth(1);
+      thirdOptionEl = OptionsEl.locator(".listbox-button__option").nth(2);
+      fourthOptionEl = OptionsEl.locator(".listbox-button__option").nth(3);
+    });
+    test("first option should be automatically selected ", async function () {
+      await expect(firstOptionEl).toHaveAttribute("aria-selected", "true");
+      await hostBtn.click();
+      await expect(secondOptionEl).toBeVisible();
+      await secondOptionEl.click();
+      await expect(secondOptionEl).toHaveAttribute("aria-selected", "true");
+    });
+    test("should auto select using kb arrow down", async function () {
+      await hostBtn.focus();
+      await hostBtn.press("Space");
+      await expect(contentEl).toBeVisible();
+      await expect(hostBtn).toHaveAttribute("aria-expanded", "true");
+      await contentEl.press("ArrowDown");
+      await expect(secondOptionEl).toHaveAttribute("aria-selected", "true");
+    });
+  });
+
+  test.describe("for selected auto select options, presented as columns", async function () {
+    test.beforeEach(async ({ page }) => {
+      containerEl = page.locator(".listbox-button").nth(12);
+      hostBtn = containerEl.locator("button");
+      contentEl = containerEl.locator(".listbox-button__listbox");
+      OptionsEl = contentEl.locator(".listbox-button__options");
+      firstOptionEl = OptionsEl.locator(".listbox-button__option").first();
+      secondOptionEl = OptionsEl.locator(".listbox-button__option").nth(1);
+      thirdOptionEl = OptionsEl.locator(".listbox-button__option").nth(2);
+      fourthOptionEl = OptionsEl.locator(".listbox-button__option").nth(3);
+    });
+    test("disabled options are skipped and navigation moves within columns ", async function () {
+      await hostBtn.focus();
+      await hostBtn.press("Space");
+      await expect(contentEl).toBeVisible();
+      await expect(hostBtn).toHaveAttribute("aria-expanded", "true");
+      await contentEl.press("ArrowDown");
+      await contentEl.press("ArrowDown");
+      await expect(thirdOptionEl).not.toHaveAttribute("aria-selected", "true");
+      await expect(fourthOptionEl).toHaveAttribute("aria-selected", "true");
     });
   });
 });
--- packages/ui/makeup-listbox/test/index.spec.js
+++ packages/ui/makeup-listbox/test/index.spec.js
@@ -59,7 +59,7 @@ test.describe("given a listbox for automatic selection", function () {
   test.beforeEach(async ({ page }) => {
     await page.goto("/ui/makeup-listbox/index.html");
 
-    containerEl = page.locator(".listbox").nth(2);
+    containerEl = page.locator(".listbox").nth(3);
     OptionsEl = containerEl.locator(".listbox__options");
     firstOptionEl = OptionsEl.getByRole("option").first();
     secondOptionEl = OptionsEl.getByRole("option").nth(1);
@@ -86,6 +86,122 @@ test.describe("given a listbox for automatic selection", function () {
     test("should not focus on disabled option", async function () {
       const disabledOptionEl = await OptionsEl.locator(".listbox__option[aria-disabled='true']").first();
       await expect(disabledOptionEl).not.toBeFocused();
+    });
+  });
+});
+
+test.describe("given a listbox with headers", function () {
+  let containerEl;
+  let OptionsEl;
+  let firstOptionEl;
+  let secondOptionEl;
+  let firstHeaderEl;
+  let secondHeaderEl;
+  let listbox;
+  let fourthElement;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/ui/makeup-listbox/index.html");
+  });
+
+  test.describe("for unselected variant", async function () {
+    test.beforeEach(async ({ page }) => {
+      containerEl = page.locator(".listbox").nth(2);
+      listbox = await containerEl.getByRole("listbox");
+      OptionsEl = containerEl.locator(".listbox__options");
+      firstOptionEl = OptionsEl.getByRole("option").first();
+      secondOptionEl = OptionsEl.getByRole("option").nth(1);
+      firstHeaderEl = containerEl.getByRole("presentation").first();
+      secondHeaderEl = containerEl.getByRole("presentation").nth(1);
+      fourthElement = OptionsEl.getByRole("option").nth(3);
+    });
+
+    test("should have the correct initial state", async function () {
+      expect(containerEl).toBeTruthy();
+      await expect(OptionsEl).toHaveAttribute("role", "listbox");
+      await expect(firstOptionEl).toHaveAttribute("role", "option");
+    });
+
+    test("should use valid aria on selection", async function () {
+      await secondOptionEl.click();
+      await expect(secondOptionEl).toHaveAttribute("aria-selected", "true");
+    });
+
+    test("should auto select using kb arrow down", async function () {
+      await OptionsEl.focus();
+      await firstOptionEl.press("ArrowDown");
+      await expect(secondOptionEl).toHaveAttribute("aria-selected", "false");
+    });
+
+    test("should not focus on disabled option", async function () {
+      const fourthOptionEl = OptionsEl.getByRole("option").nth(3);
+      await OptionsEl.focus();
+      await firstOptionEl.press("ArrowDown");
+      await secondHeaderEl.press("ArrowDown");
+      const disabledOptionEl = await OptionsEl.locator(".listbox__option[aria-disabled='true']").first();
+      await expect(disabledOptionEl).not.toBeFocused();
+      await expect(fourthOptionEl).toHaveClass("listbox__option listbox__option--active");
+    });
+
+    test("should have aria-describedby value that references the header element", async function () {
+      const firstHeaderId = await firstHeaderEl.getAttribute("id");
+      const secondHeaderId = await secondHeaderEl.getAttribute("id");
+      await expect(firstOptionEl).toHaveAttribute("aria-describedby", firstHeaderId);
+      await expect(secondOptionEl).toHaveAttribute("aria-describedby", firstHeaderId);
+      const thirdOptionEl = OptionsEl.getByRole("option").nth(2);
+      await expect(thirdOptionEl).toHaveAttribute("aria-describedby", firstHeaderId);
+      const fourthOptionEl = OptionsEl.getByRole("option").nth(3);
+      await expect(fourthOptionEl).toHaveAttribute("aria-describedby", secondHeaderId);
+      const fifthOptionEl = OptionsEl.getByRole("option").nth(4);
+      await expect(fifthOptionEl).toHaveAttribute("aria-describedby", secondHeaderId);
+      const sixthOptionEl = OptionsEl.getByRole("option").nth(5);
+      await expect(sixthOptionEl).toHaveAttribute("aria-describedby", secondHeaderId);
+    });
+  });
+
+  test.describe("for selected variant with automatic selection for grouped headers", async function () {
+    test.beforeEach(async ({ page }) => {
+      containerEl = page.locator(".listbox").nth(7);
+      listbox = await containerEl.getByRole("listbox");
+      OptionsEl = containerEl.locator(".listbox__options");
+      firstOptionEl = OptionsEl.getByRole("option").first();
+      secondOptionEl = OptionsEl.getByRole("option").nth(1);
+      firstHeaderEl = containerEl.getByRole("presentation").first();
+      secondHeaderEl = containerEl.getByRole("presentation").nth(1);
+      fourthElement = OptionsEl.getByRole("option").nth(3);
+    });
+    test("should use valid aria on selection", async function () {
+      await expect(firstOptionEl).toHaveAttribute("aria-selected", "true");
+      await secondOptionEl.click();
+      await expect(secondOptionEl).toHaveAttribute("aria-selected", "true");
+    });
+
+    test("should auto select using kb arrow down", async function () {
+      await OptionsEl.focus();
+      await firstOptionEl.press("ArrowDown");
+      await expect(secondOptionEl).toHaveAttribute("aria-selected", "true");
+    });
+  });
+
+  test.describe("for listbox options presented as columns", async function () {
+    test.beforeEach(async ({ page }) => {
+      containerEl = page.locator(".listbox").nth(8);
+      listbox = await containerEl.getByRole("listbox");
+      OptionsEl = containerEl.locator(".listbox__options");
+      firstOptionEl = OptionsEl.getByRole("option").first();
+      secondOptionEl = OptionsEl.getByRole("option").nth(1);
+      firstHeaderEl = containerEl.getByRole("presentation").first();
+      secondHeaderEl = containerEl.getByRole("presentation").nth(1);
+      fourthElement = OptionsEl.getByRole("option").nth(3);
+    });
+    test("should auto select using kb arrow down", async function () {
+      await OptionsEl.focus();
+      await OptionsEl.press("ArrowDown");
+      await OptionsEl.press("ArrowDown");
+      fourthElement = OptionsEl.getByRole("option").nth(3);
+      await expect(fourthElement).toHaveAttribute("aria-selected", "true");
+      const fourthElementId = await fourthElement.getAttribute("id");
+      await expect(listbox).toHaveAttribute("aria-activedescendant", fourthElementId);
     });
   });
 });

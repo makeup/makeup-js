5d6bebe2aa35aca690e1f88b7604bc359d3c23d3
Ian McBurnie <38065+ianmcburnie@users.noreply.github.com>
2026-02-05 12:35:56 -0800

    fix: bad github based commit

--- packages/core/makeup-expander/src/index.js
+++ packages/core/makeup-expander/src/index.js
@@ -135,20 +135,14 @@ export default class {
     this._focusExitListener = onFocusExit.bind(this);
     this._mouseLeaveListener = onMouseLeave.bind(this);
 
-if (this.options.useAriaExpanded === true) {
-  const initialAriaExpanded = this.hostEl.getAttribute("aria-expanded");
-  this._expanded = initialAriaExpanded === "true";
-  if (initialAriaExpanded === null) {
-    this.hostEl.setAttribute("aria-expanded", "false");
-  }
-} else {
-  this._expanded = false;
-}
-      this.options.useAriaExpanded === true ? this.hostEl.getAttribute("aria-expanded") : null;
-    this._expanded = initialAriaExpanded === "true";
-
-    if (this.options.useAriaExpanded === true && this.hostEl.getAttribute("aria-expanded") === null) {
-      this.hostEl.setAttribute("aria-expanded", "false");
+    if (this.options.useAriaExpanded === true) {
+      const initialAriaExpanded = this.hostEl.getAttribute("aria-expanded");
+      this._expanded = initialAriaExpanded === "true";
+      if (initialAriaExpanded === null) {
+        this.hostEl.setAttribute("aria-expanded", "false");
+      }
+    } else {
+      this._expanded = false;
     }
 
     if (this.options.ariaControls === true) {

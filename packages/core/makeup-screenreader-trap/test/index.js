3a20ab06467c8d1b9bf545769e661e16aa9312f7
Ian McBurnie <38065+ianmcburnie@users.noreply.github.com>
2026-03-28 14:07:14 -0700

    test(screenreader-trap): increase coverage

--- packages/core/makeup-screenreader-trap/test/index.js
+++ packages/core/makeup-screenreader-trap/test/index.js
@@ -115,3 +115,99 @@ testData.forEach((data) => {
     });
   });
 });
+
+describe("Module", () => {
+  describe("given a DOM with siblings", () => {
+    describe("when trap is activated with useHiddenProperty: true", () => {
+      let trapEl;
+
+      beforeEach(() => {
+        document.body.innerHTML = '<div></div><button class="trap"></button><div></div>';
+        trapEl = document.querySelector(".trap");
+        screenreaderTrap.trap(trapEl, { useHiddenProperty: true });
+      });
+
+      it("should set hidden property to true on siblings", () => {
+        expect(document.querySelectorAll("[hidden]").length).toBe(2);
+      });
+
+      it("should not add aria-hidden attribute to any element", () => {
+        expect(document.querySelectorAll("[aria-hidden]").length).toBe(0);
+      });
+
+      it("should set hidden property to false on trapped element", () => {
+        expect(trapEl.hidden).toBe(false);
+      });
+    });
+
+    describe("when trap is activated then deactivated with useHiddenProperty: true", () => {
+      let trapEl;
+
+      beforeEach(() => {
+        document.body.innerHTML = '<div></div><button class="trap"></button><div></div>';
+        trapEl = document.querySelector(".trap");
+        screenreaderTrap.trap(trapEl, { useHiddenProperty: true });
+        screenreaderTrap.untrap();
+      });
+
+      it("should set hidden property to false on siblings", () => {
+        expect(document.querySelectorAll("[hidden]").length).toBe(0);
+      });
+    });
+  });
+
+  describe("given a DOM with pre-hidden siblings", () => {
+    describe("when trap is activated then deactivated with useHiddenProperty: true", () => {
+      let trapEl;
+
+      beforeEach(() => {
+        document.body.innerHTML = '<div hidden></div><button class="trap"></button><div hidden></div>';
+        trapEl = document.querySelector(".trap");
+        screenreaderTrap.trap(trapEl, { useHiddenProperty: true });
+        screenreaderTrap.untrap();
+      });
+
+      it("should restore pre-existing hidden property on siblings", () => {
+        expect(document.querySelectorAll("[hidden]").length).toBe(2);
+      });
+    });
+  });
+
+  describe("given a DOM with a main landmark", () => {
+    let trapEl;
+
+    beforeEach(() => {
+      document.body.innerHTML = '<main role="main"></main><button class="trap"></button>';
+      trapEl = document.querySelector(".trap");
+    });
+
+    describe("when trap is activated", () => {
+      beforeEach(() => {
+        screenreaderTrap.trap(trapEl);
+      });
+
+      it("should set role=presentation on main landmark", () => {
+        expect(document.querySelector("main").getAttribute("role")).toBe("presentation");
+      });
+    });
+
+    describe("when trap is activated then deactivated", () => {
+      beforeEach(() => {
+        screenreaderTrap.trap(trapEl);
+        screenreaderTrap.untrap();
+      });
+
+      it("should set role=main on main landmark", () => {
+        expect(document.querySelector("main").getAttribute("role")).toBe("main");
+      });
+    });
+  });
+
+  describe("given no active trap", () => {
+    describe("when untrap is called", () => {
+      it("should not throw", () => {
+        expect(() => screenreaderTrap.untrap()).not.toThrow();
+      });
+    });
+  });
+});
